56b4edf1e7319bba572ae484c663fcf266b97aa4
katuen <rikusama112@gmail.com>
2025-10-16 15:59:05 +0900

    K24045の日記
    日記を書きました

---
+++ diaries/KatorikuDiary.py
@@ -0,0 +1,9 @@
+from diaries.AbstractDiary import AbstractDiary
+class KatorikuDiary(AbstractDiary):
+    def get_date(self):
+        return "2024-10-16"
+    def get_summary(self):
+        return """徹夜明けだったので眠りそうになりましたが、なんとか耐えました"""
+    def get_author(self):
+        return "Katoriku"
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from diaries.DiarySample import DiarySample
 from diaries.HoshimiDiary import HoshimiDiary
 from diaries.KarotoDiary import KarotoDiary
-
+from diaries.KatorikuDiary import KatorikuDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            HoshimiDiary(), 
-           KarotoDiary()]
+           KarotoDiary(),
+           KatorikuDiary()]
 
 for d in diaries:
     print("---------------------------------")
